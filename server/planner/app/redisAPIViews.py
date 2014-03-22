f31447009fa10f4a1f33a3c9ece72e8a49c9d3e3
chhe <chhe@ebay.com>
2014-03-22 17:30:50 -0400

    added redisApiviews

---
+++ server/planner/app/redisAPIViews.py
@@ -0,0 +1,21 @@
+'''
+Created on Mar 22, 2014
+
+@author: chhe
+'''
+import os
+import redis
+
+from flask import Flask
+from flask import Response
+from flask import json
+from app import app
+
+
+@app.route("/resturants/random")
+def clouds():
+    aKey = app.redis.randomkey();
+    data = app.redis.hgetall(aKey);
+    newdata = [json.loads(val) for key,val in data.iteritems()];    
+    resp = Response(json.dumps(newdata) , status=200, mimetype='application/json')
+    return resp;
