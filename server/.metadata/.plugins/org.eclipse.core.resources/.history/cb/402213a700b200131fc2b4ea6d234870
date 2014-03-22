5f5f8bd39fbaa484358815763f6b67f1d641839c
chhe <chhe@ebay.com>
2014-03-22 17:07:58 -0400

    finished resturant random

---
+++ server/.metadata/.plugins/org.eclipse.core.resources/.history/cb/402213a700b200131fc2b4ea6d234870
@@ -0,0 +1,8 @@
+'''
+Created on Mar 22, 2014
+
+@author: chhe
+'''
+#!flask/bin/python
+from app import app
+app.run(debug = True)
--- server/planner/app/views.py
+++ server/planner/app/views.py
@@ -6,16 +6,20 @@ Created on Mar 22, 2014
 import os
 import redis
 
-from app import app
+from flask import Flask
 from flask import Response
 from flask import json
+from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
-
-@app.route('/json')
-def readRedis():
-    
+@app.route("resturants/random.json")
+def clouds():
+    aKey = app.redis.randomkey();
+    data = app.redis.hgetall(aKey);
+    newdata = [json.loads(val) for key,val in data.iteritems()];    
+    resp = Response(json.dumps(newdata) , status=200, mimetype='application/json')
+    return resp;
