48706d42fb14b684c839caf5e27f25512a3e4748
Keshi Dai <keshi.dai@gmail.com>
2014-03-31 23:54:02 -0400

    Add job to dump restaurant dataset

--- batch-scripts/redis_data_load.py
+++ batch-scripts/redis_data_load.py
@@ -1,3 +1,4 @@
+from ast import literal_eval as make_tuple
 import argparse
 import json
 import redis
@@ -17,7 +18,7 @@ def parse_restaurant_info(line):
     entity['phone'] = parts[6]
     try:
         entity['type'] = int(parts[7])
-    except:
+    except ValueError:
         entity['type'] = 0
     try:
         entity['insp_scr'] = int(parts[11])
@@ -53,12 +54,41 @@ def import_restaurant_data(args):
                 print '%d items has been inserted' % count
 
 
-def print_unique_addresses(args):
-    datafile = args.data
-    uniq_set = set()
-    with open(datafile, 'r') as f:
-        count = 0
+def generate_data_set(args):
+    data_file = args.data
+    location_file = args.locations
+
+    locations = dict()
+    with open(location_file, 'r') as f:
         for line in f:
+            addr, loc = line.strip().split(':')
+            locations[addr.strip()] = make_tuple(loc.strip())
+
+    uniq_set = set()
+    with open(data_file, 'r') as f:
+        lines = (line.strip() for line in f)
+        count = 0
+        for line in lines:
+            count += 1
+            if count == 1:
+                continue
+            info = parse_restaurant_info(line)
+            key = '%s, %s' % (info['addr'], info['zipcode'])
+            signature = '%s:%s:%s' % (info['name'], info['addr'], info['zipcode'])
+            if key in locations and signature not in uniq_set:
+                info['location'] = locations[key]
+                json_obj = json.dumps(info, ensure_ascii=False)
+                uniq_set.add(signature)
+                print json_obj
+
+
+def print_unique_addresses(args):
+    data_file = args.data
+    uniq_set = set()
+    with open(data_file, 'r') as f:
+        lines = (line.strip() for line in f)
+        count = 0
+        for line in lines:
             count += 1
             if count == 1:
                 continue
@@ -71,16 +101,19 @@ def print_unique_addresses(args):
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--run', type=str, choices=['import', 'print', 'generate'], required=True)
     parser.add_argument('--data', type=str, required=True)
-    parser.add_argument('--host', type=str, required=True)
+    parser.add_argument('--locations', type=str, default='locations')
+    parser.add_argument('--host', type=str, default='127.0.0.1')
     parser.add_argument('--port', type=int, default=6379)
-    parser.add_argument('--run', type=str, choices=['import', 'print'], required=True)
 
     args = parser.parse_args()
     if args.run == 'import':
         import_restaurant_data(args)
     elif args.run == 'print':
         print_unique_addresses(args)
+    elif args.run == 'generate':
+        generate_data_set(args)
 
 
 if __name__ == '__main__':
