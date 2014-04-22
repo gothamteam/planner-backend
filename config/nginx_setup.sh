ea0cd75bf5975498d02920b26a3267be3bd9b291
Keshi Dai <keshi.dai@gmail.com>
2014-04-21 23:50:00 -0400

    Fix setup script

--- config/nginx_setup.sh
+++ config/nginx_setup.sh
@@ -3,10 +3,11 @@
 # install nginx
 # mac: brew install nginx, config file /usr/local/etc/nginx/nginx.conf
 sudo apt-get install nginx
+cp config/nginx.conf /etc/nginx/
 sudo service nginx start
 # sudo service nginx stop
 # update-rc.d nginx defaults
 
-cp config/nginx.conf /etc/nginx/
-uwsgi --socket 127.0.0.1:8080 --protocol=http -w wsgi:app
+cd server/
+uwsgi --socket 0.0.0.0:8080 --protocol=http -w wsgi:app
 
