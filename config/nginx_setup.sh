677bc28f497a7f84dbb0e502fe64f80ef4b005ef
Keshi Dai <keshi.dai@gmail.com>
2014-04-21 23:15:20 -0400

    Add uwsgi nginx config

---
+++ config/nginx_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# install nginx
+# mac: brew install nginx, config file /usr/local/etc/nginx/nginx.conf
+sudo apt-get install nginx
+sudo service nginx start
+# sudo service nginx stop
+# update-rc.d nginx defaults
+
